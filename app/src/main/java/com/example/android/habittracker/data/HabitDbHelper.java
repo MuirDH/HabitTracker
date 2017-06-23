a42ef8d767fc0c21fc10bfeaaa6ffa1f5462296d
Muir Halleron <muir.halleron@gmail.com>
2017-06-23 11:14:37 +0100

    Add logic to the onUpgrade() method in the SQLiteOpenHelper subclass.

--- app/src/main/java/com/example/android/habittracker/data/HabitDbHelper.java
+++ app/src/main/java/com/example/android/habittracker/data/HabitDbHelper.java
@@ -34,19 +34,20 @@ public class HabitDbHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    // Create a String that contains the SQL statement to create the habits table
+    private static final String SQL_CREATE_HABITS_TABLE = "CREATE TABLE "
+            + HabitEntry.TABLE_NAME + " ("
+            + HabitEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + HabitEntry.COLUMN_HABIT + " TEXT NOT NULL, "
+            + HabitEntry.COLUMN_EXTRA_INFO + " TEXT, "
+            + HabitEntry.COLUMN_FREQUENCY + " INTEGER NOT NULL, "
+            + HabitEntry.COLUMN_TIME + " INTEGER NOT NULL DEFAULT 1);";
+
     /**
      * This is called when the database is created for the first time.
      */
     @Override
     public void onCreate(SQLiteDatabase db) {
-
-        // Create a String that contains the SQL statement to create the habits table
-        String SQL_CREATE_HABITS_TABLE = "CREATE TABLE " + HabitEntry.TABLE_NAME + " ("
-                + HabitEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + HabitEntry.COLUMN_HABIT + " TEXT NOT NULL, "
-                + HabitEntry.COLUMN_EXTRA_INFO + " TEXT, "
-                + HabitEntry.COLUMN_FREQUENCY + " INTEGER NOT NULL, "
-                + HabitEntry.COLUMN_TIME + " INTEGER NOT NULL DEFAULT 1);";
 
         // Execute the SQL statement
         db.execSQL(SQL_CREATE_HABITS_TABLE);
@@ -58,7 +59,15 @@ public class HabitDbHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
-        // The database is still at version 1, so there's nothing to do here for now.
+        /*
+         * Check the versions and update where applicable. For now, this just returns the current
+         * database table, which is better than having just an empty function. In future, this code
+         * can be changed to handle any changes to the database that we need to make.
+         */
+
+        if (oldVersion < 2 || oldVersion < 3) {
+            db.execSQL(SQL_CREATE_HABITS_TABLE);
+        }
 
     }
 
